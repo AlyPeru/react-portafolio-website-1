7081e7f5616c842f5404e9d6865068d428965d74
alidacch@gmail.com <alidacch@gmail.com>
2022-04-12 13:01:20 -0500

    Listo para desplegar

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -4,6 +4,7 @@ import './nav.css';
 import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
 import {BiBook, BiMessageSquareDetail} from 'react-icons/bi'
 import {ImMakeGroup} from 'react-icons/im';
+import {MdOutlineReviews} from 'react-icons/md';
 import { useState } from 'react';
 
 
@@ -17,6 +18,7 @@ const Nav = () => {
       <a href="#about" onClick={()=>(setActiveNav('#about'))} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
       <a href="#experience" onClick={()=>(setActiveNav('#experience'))} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
       <a href="#portfolio" onClick={()=>(setActiveNav('#portfolio'))} className={activeNav === '#portfolio' ? 'active' : ''}><ImMakeGroup/></a>
+      <a href="#testimonials" onClick={()=>(setActiveNav('#testimonials'))} className={activeNav === '#testimonials' ? 'active' : ''}><MdOutlineReviews/></a>
       <a href="#contact" onClick={()=>(setActiveNav('#contact'))} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
 
       
